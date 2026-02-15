c8f0f88303e48d42db7123f58a0716775bd73d9e
DariaDov <dovhaniuk.daria@lll.kpi.ua>
2026-02-15 13:38:23 +0200

    Лабораторна робота 1

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <thread>
 #include <cstdlib>
+#include <vector>
+#include <algorithm>
 
 //var 19
 
 const int n = 10;
 const int max_num = 100;
+const int threads_num = 10;
 
-void fillRandom(int (&matrix)[n][n]) {
+int matrix[n][n]; 
+
+void fillRandom() {
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             matrix[i][j] = rand() % max_num;
@@ -15,21 +20,37 @@ void fillRandom(int (&matrix)[n][n]) {
     }
 }
 
-void printMatrix(int (&matrix)[n][n])
+void printMatrix()
 {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            std::cout << matrix[i][j] << " ";
+            std::cout << matrix[j][i] << " ";
         }
         std::cout << std::endl;
     }
+    std::cout << std::endl;
+}
+
+void findMax(const int tread_id) {
+    int max_val_id = std::max_element(&matrix[tread_id][0], &matrix[tread_id][0] + n) - &matrix[tread_id][0];
+    std::swap(matrix[tread_id][tread_id], matrix[tread_id][max_val_id]);
 }
  
 int main() {
-    int matrix[n][n]; 
+    fillRandom();
+    printMatrix();
 
-    fillRandom(matrix);
-    printMatrix(matrix);
+    std::vector<std::thread> threads;
+ 
+	for (int i = 0; i < threads_num; i++)	{
+		threads.push_back(std::thread(findMax, i));
+	}
 
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    printMatrix();
+    
     return 0;
 }
