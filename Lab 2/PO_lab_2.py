96998de18fb1704980e11b46b12ed8f0af1f8bf9
DariaDov <dovhaniuk.daria@lll.kpi.ua>
2026-02-24 19:19:29 +0200

    Lab 2 is almost done

---
+++ Lab 2/PO_lab_2.py
@@ -0,0 +1,32 @@
+import subprocess
+import matplotlib.pyplot as plt
+
+npoints = []
+timepoints = []
+threads_num = 6
+n_range = [10000, 100000, 1000000, 10000000, 100000000, 1000000000]
+
+for n in n_range:
+    result = subprocess.run([f"/home/daria/ПО/PO_lab_2/build/PO_lab_2 {n} {threads_num}"], shell=True, check=False, capture_output=True, text=True)
+    print(result)
+        
+    npoints.append(n)
+    timepoints.append(int(result.stdout.split('\n')[0]))
+
+plt.plot(npoints, timepoints, 'C0', label="mutex")
+
+npoints = []
+timepoints = []
+
+for n in n_range:
+    result = subprocess.run([f"/home/daria/ПО/PO_lab_2/build/PO_lab_2_1 {n} {threads_num}"], shell=True, check=False, capture_output=True, text=True)
+    print(result)
+        
+    npoints.append(n)
+    timepoints.append(int(result.stdout.split('\n')[0]))
+
+plt.plot(npoints, timepoints, 'C3', label="atomic")
+
+plt.legend() 
+plt.xscale('log')
+plt.show()
---
+++ Lab 2/PO_lab_2_1.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <thread>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+#include <mutex>
+#include <atomic>
+
+//var 19: Знайти кількість елементів, більших за 20, а також найбільше таке число.
+
+int threads_num = 6; //to do
+
+std::vector<int> numbers;
+
+const int threshold = 20;
+int n = 100000000;
+const int max_num = 40;
+int cnt = 0;
+int max_el = 0;
+
+std::atomic<int> at_cnt(0);
+std::atomic<int> at_max_el(0);
+
+void fillRandom() {
+ for (int i = 0; i < n; ++i) {
+        numbers.push_back(rand() % max_num);
+    }
+}
+
+void printVector()
+{
+    for (auto i : numbers) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
+
+void noParTask () {
+    max_el = *std::max_element(numbers.begin(), numbers.end());
+    if (max_el <= threshold) {
+        max_el = 0;
+        return;
+    }
+    cnt = std::count_if(numbers.begin(), numbers.end(), [](int num) {
+        return num > threshold;
+    });
+}
+
+void atomicTask (const int start_pos, const int end_pos) {
+    // for (int i = start_pos; i < end_pos; ++i) {
+    //     if (numbers[i] > 20) {
+    //         const int previous = at_cnt.fetch_add(1); // (..., std::memory_order_relaxed) ?
+
+    //         int current_max = at_max_el.load();
+    //         while (numbers[i]  > current_max && !at_max_el.compare_exchange_weak(current_max, numbers[i] )) { }
+    //     }
+    // }
+
+    int local_cnt = 0;
+
+    int local_max_el = *std::max_element(numbers.begin() + start_pos, numbers.begin() + end_pos);
+    if (at_max_el <= threshold) {
+        return;
+    }
+
+    at_cnt = std::count_if(numbers.begin() + start_pos, numbers.begin() + end_pos, [](int num) {
+        return num > threshold;
+    });
+    
+    const int previous = at_cnt.fetch_add(1); // (..., std::memory_order_relaxed) ?
+
+    int current_max = at_max_el.load();
+    while (local_max_el  > current_max && !at_max_el.compare_exchange_weak(current_max, local_max_el)) { }
+    
+}
+ 
+// p1 - n, p2 - threads_num
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        n = atoi(argv[1]);
+        threads_num = atoi(argv[2]);
+    }
+
+    numbers.reserve(n);
+    fillRandom();
+    // printVector();
+
+    if (threads_num == 1) {
+        auto start1 = std::chrono::steady_clock::now();
+        noParTask();
+        auto end1 = std::chrono::steady_clock::now();
+        auto diff1 = end1 - start1;
+        if (argc == 1) {
+            std::cout << "Кількість елементів більша за 20: " << cnt << std::endl;
+            std::cout << "Найбільший елемент: " << max_el << std::endl;
+            std::cout << "Час без використання паралелізації: " << std::chrono::duration_cast<std::chrono::microseconds>(diff1).count() << std::endl;
+        }
+        else {
+            std::cout << std::chrono::duration_cast<std::chrono::microseconds>(diff1).count() << std::endl;
+        }
+    }
+    else {
+        std::vector<std::thread> threads;
+    
+        int start_threads = std::min(n, threads_num);
+        threads.reserve(start_threads);
+
+        auto start2 = std::chrono::steady_clock::now();
+
+        int start_pos = 0;
+        int end_pos = 0;
+        int step = n / start_threads;
+        for (int i = 0; i < start_threads; i++) {
+            start_pos = i * step;
+            end_pos = (i == start_threads - 1) ? n : start_pos + step;
+
+            threads.push_back(std::thread(atomicTask, start_pos, end_pos));
+        }
+
+        for (int i = 0; i < start_threads; i++) {
+            threads[i].join();
+        }
+
+        auto end2 = std::chrono::steady_clock::now();
+        auto diff2 = end2 - start2;
+        if (argc == 1) {
+            std::cout << "Кількість елементів більша за 20: " << cnt << std::endl;
+            std::cout << "Найбільший елемент: " << max_el << std::endl;
+            std::cout << "Час atomic: " << std::chrono::duration_cast<std::chrono::microseconds>(diff2).count() << std::endl;
+        }
+        else {
+            std::cout << std::chrono::duration_cast<std::chrono::microseconds>(diff2).count() << std::endl;
+        }
+    }
+    
+    return 0;
+}
