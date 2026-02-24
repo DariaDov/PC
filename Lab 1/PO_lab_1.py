0d824eaf01a3306c6e8a26c337f17c3c19ba3e40
DariaDov <dovhaniuk.daria@lll.kpi.ua>
2026-02-24 19:23:17 +0200

    Full lab 1

---
+++ Lab 1/PO_lab_1.py
@@ -0,0 +1,19 @@
+import subprocess
+import matplotlib.pyplot as plt
+
+for n in [10, 50, 100, 500, 1000, 10000, 20000]:
+    threadpoints = []
+    timepoints = []
+
+    for threads_num in [1, 3, 6, 12, 24, 48, 96, 192]:
+        result = subprocess.run([f"/home/daria/ПО/PO_lab_1/build/PO_lab_1 {n} {threads_num}"], shell=True, check=False, capture_output=True, text=True)
+        print(result)
+        
+        threadpoints.append(threads_num)
+        timepoints.append(int(result.stdout.split('\n')[0]))
+
+    plt.plot(threadpoints, timepoints,  label=f'{n}')
+
+plt.legend() 
+# plt.xscale('log')
+plt.show()
