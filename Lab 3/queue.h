b7eeb933ac9275a7c6c0f659ac5ad14aac84ead8
DariaDov <dovhaniuk.daria@lll.kpi.ua>
2026-03-29 21:40:46 +0300

    Queue added

---
+++ Lab 3/queue.h
@@ -0,0 +1,49 @@
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <shared_mutex>
+
+template <typename T>
+class MyQueue {
+public:
+    MyQueue() = default;
+    ~MyQueue() { clear(); }
+
+    bool empty() const {
+        std::shared_lock lock(mtx);
+        return task_queue.empty();
+    }
+
+    int size() const {
+        std::shared_lock lock(mtx);
+        return task_queue.size();
+    }
+
+    void clear() {
+        std::unique_lock lock(mtx);
+        
+        while (!task_queue.empty()) {
+            task_queue.pop();
+        }
+    }
+
+    bool pop(T& task) {
+        std::unique_lock lock(mtx);
+        if (task_queue.empty()) {
+            return false;
+        }
+
+        task = std::move(task_queue.front());
+        task_queue.pop();
+        return true;
+    }
+
+    template <typename... Args>
+    void emplace(Args&&... args) {
+        std::unique_lock lock(mtx);
+        task_queue.emplace(std::forward<Args>(args)...);
+    }
+private:
+    std::queue<T> task_queue;
+    mutable std::shared_mutex mtx;
+};
