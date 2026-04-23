8f3ffd466fa9a15eda798750f1383100ded891ad
DariaDov <dovhaniuk.daria@lll.kpi.ua>
2026-04-23 21:22:59 +0300

    destructor changed

--- Lab 4/task.h
+++ Lab 4/task.h
@@ -13,6 +13,7 @@ public:
     int *matrix;
     int thread_num;
     std::atomic<int> progress;
+    std::atomic<bool> stop_flag = false;
     std::vector<std::thread> threads;
 
     Task(int matrix_size, int *matrix, int thread_num) {
@@ -25,7 +26,9 @@ public:
 
         this->thread_num = std::min(this->thread_num, this->matrix_size);
     };
-    ~Task() {};
+    ~Task() {
+        finish();
+    };
     
     void run() {
         threads.reserve(thread_num);
@@ -44,6 +47,7 @@ public:
     }
 
     void finish() {
+        stop_flag = true;
         for (int i = 0; i < threads.size(); i++) {
             threads[i].join();
         }
@@ -52,6 +56,7 @@ public:
 private:
     static void findMax(int start_pos, Task * task) {
         for (int c = start_pos * task->matrix_size; c < task->matrix_size * task->matrix_size; c += task->thread_num * task->matrix_size) {
+            if (task->stop_flag) return;
             int max_val_id = std::max_element(task->matrix + c, task->matrix + c + task->matrix_size) - task->matrix - c;
             std::swap(task->matrix[c + c / task->matrix_size], task->matrix[max_val_id]);
             
